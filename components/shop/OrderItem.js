1c54ac347d5e0a27545af010d46d738e9a80a8ba
Peter Kim <peter.minho.kim@gmail.com>
2020-12-14 21:43:06 -0500

    Adds functionality for show details button

--- components/shop/OrderItem.js
+++ components/shop/OrderItem.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
-import Colors from '../../constants/Colors';
 
+import Colors from '../../constants/Colors';
 import CartItem from './CartItem';
 
 const OrderItem = (props) => {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
     <View style={styles.orderItem}>
       <View style={styles.summary}>
         <Text style={styles.totalAmount}>${props.amount.toFixed(2)}</Text>
         <Text style={styles.date}>{props.date}</Text>
       </View>
-      <Button title="Show Details" color={Colors.primary} />
+      <Button
+        title={showDetails ? 'Hide Details' : 'Show Details'}
+        color={Colors.primary}
+        onPress={() => {
+          setShowDetails((prevState) => !prevState);
+        }}
+      />
+      {showDetails && (
+        <View style={styles.detailItems}>
+          {props.items.map((cartItem) => (
+            <CartItem
+              key={cartItem.productId}
+              quantity={cartItem.quantity}
+              amount={cartItem.sum}
+              title={cartItem.productTitle}
+            />
+          ))}
+        </View>
+      )}
     </View>
   );
 };
@@ -44,6 +64,9 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontFamily: 'open-sans',
     color: '#888'
+  },
+  detailItems: {
+    width: '100%'
   }
 });
 
