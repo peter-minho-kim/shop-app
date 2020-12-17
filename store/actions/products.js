e550a11f92b3a6a286638efd7c132506ef0b943f
Peter Kim <peter.minho.kim@gmail.com>
2020-12-17 17:51:45 -0500

    Update and delete products

--- store/actions/products.js
+++ store/actions/products.js
@@ -43,9 +43,18 @@ export const fetchProducts = () => {
 };
 
 export const deleteProduct = (productId) => {
-  return {
-    type: DELETE_PRODUCT,
-    pid: productId
+  return async (dispatch) => {
+    await fetch(
+      `https://rn-complete-guide-84e3a-default-rtdb.firebaseio.com/products/${productId}.json`,
+      {
+        method: 'DELETE'
+      }
+    );
+
+    dispatch({
+      type: DELETE_PRODUCT,
+      pid: productId
+    });
   };
 };
 
@@ -83,13 +92,30 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return {
-    type: UPDATE_PRODUCT,
-    pid: id,
-    productData: {
-      title,
-      description,
-      imageUrl
-    }
+  return async (dispatch) => {
+    await fetch(
+      `https://rn-complete-guide-84e3a-default-rtdb.firebaseio.com/products/${id}.json`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl
+        })
+      }
+    );
+
+    dispatch({
+      type: UPDATE_PRODUCT,
+      pid: id,
+      productData: {
+        title,
+        description,
+        imageUrl
+      }
+    });
   };
 };
