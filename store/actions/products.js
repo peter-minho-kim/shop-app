95096b161961e10aaad613658937ba0fc5140be5
Peter Kim <peter.minho.kim@gmail.com>
2020-12-17 21:29:34 -0500

    Handles additional errors

--- store/actions/products.js
+++ store/actions/products.js
@@ -44,12 +44,16 @@ export const fetchProducts = () => {
 
 export const deleteProduct = (productId) => {
   return async (dispatch) => {
-    await fetch(
+    const response = await fetch(
       `https://rn-complete-guide-84e3a-default-rtdb.firebaseio.com/products/${productId}.json`,
       {
         method: 'DELETE'
       }
     );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong');
+    }
 
     dispatch({
       type: DELETE_PRODUCT,
@@ -93,7 +97,7 @@ export const createProduct = (title, description, imageUrl, price) => {
 
 export const updateProduct = (id, title, description, imageUrl) => {
   return async (dispatch) => {
-    await fetch(
+    const response = await fetch(
       `https://rn-complete-guide-84e3a-default-rtdb.firebaseio.com/products/${id}.json`,
       {
         method: 'PATCH',
@@ -107,6 +111,10 @@ export const updateProduct = (id, title, description, imageUrl) => {
         })
       }
     );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong');
+    }
 
     dispatch({
       type: UPDATE_PRODUCT,
