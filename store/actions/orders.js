53ddfeb5c0dbd67d66379a0f4d244edef2b12fa5
Peter Kim <peter.minho.kim@gmail.com>
2020-12-18 22:16:22 -0500

    using the token

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -32,10 +32,11 @@ export const fetchOrders = () => {
 };
 
 export const addOrder = (cartItems, totalAmount) => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
     const date = new Date();
     const response = await fetch(
-      'https://rn-complete-guide-84e3a-default-rtdb.firebaseio.com/orders/u1.json',
+      `https://rn-complete-guide-84e3a-default-rtdb.firebaseio.com/orders/u1.json?auth=${token}`,
       {
         method: 'POST',
         headers: {
--- store/actions/products.js
+++ store/actions/products.js
@@ -43,9 +43,10 @@ export const fetchProducts = () => {
 };
 
 export const deleteProduct = (productId) => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
     const response = await fetch(
-      `https://rn-complete-guide-84e3a-default-rtdb.firebaseio.com/products/${productId}.json`,
+      `https://rn-complete-guide-84e3a-default-rtdb.firebaseio.com/products/${productId}.json?auth=${token}`,
       {
         method: 'DELETE'
       }
@@ -63,9 +64,10 @@ export const deleteProduct = (productId) => {
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
     const response = await fetch(
-      'https://rn-complete-guide-84e3a-default-rtdb.firebaseio.com/products.json',
+      `https://rn-complete-guide-84e3a-default-rtdb.firebaseio.com/products.json?auth=${token}`,
       {
         method: 'POST',
         headers: {
@@ -96,9 +98,10 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
     const response = await fetch(
-      `https://rn-complete-guide-84e3a-default-rtdb.firebaseio.com/products/${id}.json`,
+      `https://rn-complete-guide-84e3a-default-rtdb.firebaseio.com/products/${id}.json?auth=${token}`,
       {
         method: 'PATCH',
         headers: {
